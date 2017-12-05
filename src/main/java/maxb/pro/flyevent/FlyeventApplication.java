9702dfc6c92fc20b9087e78a53161ada8acb8ba2
MaxB <maxbpro2009@gmail.com>
2017-12-05 19:46:52 +0300

    Created a Spring boot project

---
+++ src/main/java/maxb/pro/flyevent/FlyeventApplication.java
@@ -0,0 +1,12 @@
+package maxb.pro.flyevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyeventApplication.class, args);
+	}
+}
---
+++ src/test/java/maxb/pro/flyevent/FlyeventApplicationTests.java
@@ -0,0 +1,16 @@
+package maxb.pro.flyevent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlyeventApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
